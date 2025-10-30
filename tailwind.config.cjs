caadf32a2b7bbd153ceb78f7038f0c894007c517
funcode0x <codeforfun0x@gmail.com>
2025-10-30 12:40:54 +0100

    Add Tailwind CSS configuration file
    Configure Tailwind CSS with custom colors.

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        baseBlue: "#0052FF",
+        darkBg: "#000b1d"
+      }
+    }
+  },
+  plugins: []
+};
